41941eb4354a5dbee353a803ae404a15e5658248
nzubezion <chukwunelozion@gmail.com>
2023-05-25 15:00:19 -0700

    task 1

--- task1.c
+++ task1.c
@@ -1,46 +1,99 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-
-#define BUFFER_SIZE 1024
+#include "shell_header.h"
 
 /**
- * main - Entry point of the simple shell program
+ * _myexit - Exits the shell
+ * @zion: Structure containing potential arguments. Used to maintain
+ *         constant function prototype.
  *
- * Description: This program creates a simple UNIX command line interpreter.
- * It displays a prompt, waits for the user to type a command, and executes it.
- * The command lines are single words with no arguments or special characters.
- * It handles errors, including the "end of file" condition.
- *
- * Return: Always 0 (Success)
+ * Return: Exits with a given exit status (0) if zion->argv[0] != "exit"
  */
-int main(void)
+int _myexit(info_t *zion)
 {
-char *inputBuffer;        /* Buffer to store user input */
-ssize_t readChars;        /* Number of characters read */
-size_t bufferSize = BUFFER_SIZE;   /* Size of the input buffer */
+int exitcheck;
 
-inputBuffer = malloc(bufferSize);  /* Allocate memory for input buffer */
-if (inputBuffer == NULL)
+if (zion->argv[1])
 {
-perror("Error: Unable to allocate inputBuffer");
-exit(EXIT_FAILURE);
+exitcheck = _erratoi(zion->argv[1]);
+if (exitcheck == -1)
+{
+zion->status = 2;
+print_error(zion, "Illegal number: ");
+_eputs(zion->argv[1]);
+_eputchar('\n');
+return (1);
+}
+zion->err_num = _erratoi(zion->argv[1]);
+return (-2);
+}
+zion->err_num = -1;
+return (-2);
 }
 
-while (1)
+/**
+ * _mycd - Changes the current directory of the process
+ * @zion: Structure containing potential arguments. Used to maintain
+ *         constant function prototype.
+ *
+ * Return: Always 0
+ */
+int _mycd(info_t *zion)
 {
-printf("$ ");  /* Display the prompt */
-readChars = getline(&inputBuffer, &bufferSize, stdin);
-if (readChars == -1)  /* Handle "end of file" condition */
+char *s, *dir, buffer[1024];
+int chdir_ret;
+
+s = getcwd(buffer, 1024);
+if (!s)
+_puts("TODO: >>getcwd failure emsg here<<\n");
+if (!zion->argv[1])
 {
-printf("\n");
-break;
+dir = _getenv(zion, "HOME=");
+if (!dir)
+chdir_ret = chdir((dir = _getenv(zion, "PWD=")) ? dir : "/");
+else
+chdir_ret = chdir(dir);
+}
+else if (_strcmp(zion->argv[1], "-") == 0)
+{
+if (!_getenv(zion, "OLDPWD="))
+{
+_puts(s);
+_putchar('\n');
+return (1);
+}
+_puts(_getenv(zion, "OLDPWD="));
+_putchar('\n');
+chdir_ret = chdir((dir = _getenv(zion, "OLDPWD=")) ? dir : "/");
+}
+else
+chdir_ret = chdir(zion->argv[1]);
+if (chdir_ret == -1)
+{
+print_error(zion, "can't cd to ");
+_eputs(zion->argv[1]);
+_eputchar('\n');
+}
+else
+{
+_setenv(zion, "OLDPWD", _getenv(zion, "PWD="));
+_setenv(zion, "PWD", getcwd(buffer, 1024));
+}
+return (0);
 }
 
-/* TODO: Handle command execution */
+/**
+ * _myhelp - Provides help information
+ * @zion: Structure containing potential arguments. Used to maintain
+ *         constant function prototype.
+ *
+ * Return: Always 0
+ */
+int _myhelp(info_t *zion)
+{
+char **arg_array;
 
-}
-
-free(inputBuffer);  /* Free allocated memory */
+arg_array = zion->argv;
+_puts("help call works. Function not implemented yet \n");
+if (0)
+_puts(*arg_array); /* temp att_unused workaround */
 return (0);
 }
